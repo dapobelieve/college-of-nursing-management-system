f44a6e9e0d4634e9b60ebb288344ae30aa3f55ed
King Belieeve <dapomichaels@gmail.com>
2019-10-10 12:45:23 +0100

    initial commit

---
+++ app/Http/Middleware/CheckRole.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Auth;
+
+
+class CheckRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @param array $roles
+     * @return mixed
+     */
+    public function handle($request, Closure $next, ...$roles)
+    {
+        if (Auth::check()) {
+            if (!$request->user()->hasRole($roles)) {
+                abort(404);
+            }
+            return $next($request);
+        }
+        // return redirect()->route('site.home');
+        // depending on the name of the route use this accordingly
+    }
+}
