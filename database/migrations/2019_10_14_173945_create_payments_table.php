b1edfa34c885c0cadb0f41f0f438476ec5185002
King Belieeve <dapomichaels@gmail.com>
2019-10-14 19:10:24 +0100

    update payments migration

--- database/migrations/2019_10_14_173945_create_payments_table.php
+++ database/migrations/2019_10_14_173945_create_payments_table.php
@@ -14,7 +14,14 @@ class CreatePaymentsTable extends Migration
     public function up()
     {
         Schema::create('payments', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
+            $table->integer('student_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('payment_modes_id')->unsigned();
+            $table->foreign('payment_modes_id')->references('id')->on('payment_modes');
+            $table->decimal('amount', 8, 2);
+            $table->enum('status', ['PAID', 'NOT PAID'])->default('NOT PAID');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
