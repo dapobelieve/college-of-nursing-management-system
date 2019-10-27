9b1d8b62cf4082825da6831bb7b020241e036ab0
Iyiola-am <adekunle3317@gmail.com>
2019-10-27 17:27:19 +0100

    Added base routes and demo pages

--- resources/views/admin/layout/nav.blade.php
+++ resources/views/admin/layout/nav.blade.php
@@ -6,8 +6,28 @@
         </button>
     </div> --}}  
     <ul>
-        <li class="active"><a href="#">
+        <li @if ($section == 'dashboard') class="active" @endif><a href="/admin">
             <i class="fa fa-home"></i> <span>Dashboard</span></a>
+        </li>
+
+        <li @if ($section == 'news') class="active" @endif><a href="/admin/news">
+            <i class="fa fa-bullhorn"></i> <span>News</span></a>
+        </li>
+
+        <li @if ($section == 'students') class="active" @endif><a href="/admin/students">
+            <i class="fa fa-group"></i> <span>Students</span></a>
+        </li>
+
+        <li @if ($section == 'admins') class="active" @endif><a href="/admin/admins">
+            <i class="fa fa-group"></i> <span>Admins</span></a>
+        </li>
+
+        <li @if ($section == 'roles') class="active" @endif><a href="/admin/roles">
+            <i class="fa fa-group"></i> <span>Roles</span></a>
+        </li>
+
+        <li @if ($section == 'settings') class="active" @endif><a href="/admin/settings">
+            <i class="fa fa-cog"></i> <span>System Settings</span></a>
         </li>
 
         {{-- <li class="submenu">
--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,12 @@ Route::get('/', function () {
 Route::view('/login', 'Auth.login');
 Route::post('/login', 'Auth\AuthController@login')->name('login');
 
-
+// The admin panel routes
 Route::group(['prefix' => '/admin'], function () {
-    Route::view('', '/admin.home');
+    Route::view('', '/admin.home', ['section' => 'dashboard']);
+    Route::view('roles', '/admin.roles', ['section' => 'roles']);
+    Route::view('admins', '/admin.admins', ['section' => 'admins']);
+    Route::view('students', '/admin.students', ['section' => 'students']);
+    Route::view('news', '/admin.news', ['section' => 'news']);
+    Route::view('system-settings', '/admin.system_settings', ['section' => 'settings']);
 });
