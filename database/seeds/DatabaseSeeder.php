f2d559ccf60042a997d3cb36f109e8e8ff907537
Iyiola-am <adekunle3317@gmail.com>
2019-11-08 20:01:20 +0100

    Modified news table

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,12 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-      //  $this->call(RoleTableSeeder::class);
-        //$this->call(StatesTableSeeder::class);
-        //$this->call(LocationsSeeder::class);
-        //$this->call(CardTableSeeder::class);
-        $this->call(DepartmentSeeder::class);
+      $this->call(RoleTableSeeder::class);
+      $this->call(StatesTableSeeder::class);
+      $this->call(LocationsSeeder::class);
+      $this->call(CardTableSeeder::class);
+      $this->call(DepartmentSeeder::class);
 
-        // $this->call(UsersTableSeeder::class);
+      // $this->call(UsersTableSeeder::class);
     }
 }
