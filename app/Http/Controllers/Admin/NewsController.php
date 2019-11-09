f6af528432349a50672054d11ebccf7cfc4a6224
Iyiola-am <adekunle3317@gmail.com>
2019-11-09 17:49:50 +0100

    Post creation feature implemented

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -4,11 +4,24 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use Illuminate\Validation\ValidationException;
+
 use App\Models\Post;
 use App\Http\Controllers\Controller;
 
 class NewsController extends Controller
 {
+    /**
+     * @var array $response Template for json response to be returned to the user for an ajax call */
+    protected $response = [
+        'ok' => false,
+        'message' => '',
+        'data' => [
+            'reload' => false,
+            'redirect' => null
+        ]
+    ];
+
     /**
      * Shows all blog posts
      * 
@@ -41,7 +54,29 @@ class NewsController extends Controller
      */
     public function handleCreate(Request $request)
     {
+        try {
+            // Validate request
+            $this->validate($request, [
+                'title' => 'required|unique:news,title|max:255',
+                'content' => 'required',
+            ]);
 
+            // Create the post
+            $post = new Post();
+            $post->title = $request->input('title');
+            $post->content = $request->input('content');
+            $post->author_id = 0; // Stub
+            $post->save();
+
+            $this->response['ok'] = true;
+            $this->response['message'] = 'Post created!';
+            $this->response['data']['redirect'] = '/admin/news';
+        } catch (ValidationException $e) {
+            $this->response['message'] = $e->errors()[0];
+        }
+
+        // Response
+        return $this->response;
     }
 
     /**
--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -32,7 +32,10 @@
                             <h5>Create New Post</h5>
                         </div>
                         <div class="widget-content">
-                            <form class="form-horizontal" action="/admin/create-post" method="post">
+                            <form class="form-horizontal ajax-form" action="/admin/create-post" method="post">
+                                {{ csrf_field() }}
+                                {{ method_field('POST') }}
+
                                 <div class="form-group">
                                     <input type="text" name="title" class="form-control" placeholder="Post Title" required>
                                 </div>
