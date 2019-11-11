752c1e7171f93d175a6e88cf311e6e455d2f3698
Dapo Michaels <dapomichaels@gmail.com>
2019-11-11 11:05:57 +0100

    fixed remanming

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,77 +1,69 @@
-@extends('layouts.app')
-
-@section('title')
-  Login - Portal
-@endsection
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}" enctype="multipart/form-data">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="example@gmail.com">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Unicorn Admin</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="/dashboard/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="/dashboard/css/font-awesome.css" />
+    <link rel="stylesheet" href="/dashboard/css/unicorn-login.css" />
+    <script type="text/javascript" src="js/respond.min.js"></script>
+</head>
+<body>
+    <div id="container">
+    <div id="logo">
+{{--        <img src="img/logo.png" alt="" />--}}
+    </div>
+    <div id="loginbox">
+        <form method="post"  action="{{ route('login') }}">
+            <p>Login</p>
+            <div class="input-group input-sm">
+                <span class="input-group-addon">
+                    <i class="fa fa-user"></i>
+                </span><input class="form-control" name="email" type="email" placeholder="Email" />
             </div>
-        </div>
+            <div class="input-group">
+                <span class="input-group-addon">
+                    <i class="fa fa-lock"></i>
+                </span>
+                <input class="form-control" name="password" type="password" placeholder="Password" />
+            </div>
+            <div class="form-actions clearfix">
+                <div class="pull-left">
+                    <a href="#registerform" class="flip-link to-register blue">Create new account</a>
+                </div>
+                <div class="pull-right">
+                    <a href="#recoverform" class="flip-link to-recover grey">Forgot password?</a>
+                </div>
+                <input type="submit" class="btn btn-block btn-primary btn-default" value="Login" />
+            </div>
+            {{csrf_field()}}
+        </form>
+        <form id="registerform" action="#">
+            <p>Enter information required to register:</p>
+            <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-user"></i></span><input class="form-control" type="text" placeholder="Enter Username" />
+            </div>
+            <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-lock"></i></span><input class="form-control" type="password" placeholder="Choose Password" />
+            </div>
+            <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-lock"></i></span><input class="form-control" type="password" placeholder="Confirm password" />
+            </div>
+            <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-envelope"></i></span><input class="form-control" type="text" placeholder="Enter E-mail address" />
+            </div>
+            <div class="form-actions clearfix">
+                <div class="pull-left">
+                    <a href="#loginform" class="grey flip-link to-login">Click to login</a>
+                </div>
+                <div class="pull-right">
+                    <a href="#recoverform" class="grey flip-link to-recover">Lost password?</a>
+                </div>
+                <input type="submit" class="btn btn-block btn-success" value="Register" />
+            </div>
+        </form>
     </div>
 </div>
-@endsection
+</body>
+</html>
