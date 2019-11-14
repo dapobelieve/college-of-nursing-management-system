b75854eedad963596843a11acecb52c76527505b
Iyiola-am <adekunle3317@gmail.com>
2019-11-14 17:45:52 +0100

    Lecturer and Student model improved

--- app/Models/Lecturer.php
+++ app/Models/Lecturer.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Lecturer extends Model
 {
-    //
+    use SoftDeletes;
+
+    /**
+     * Get the user model related with the lecturer
+     */
+    public function user()
+    {
+        $this->morphOne('App\User', 'userable');
+    }
 }
--- app/Models/Student.php
+++ app/Models/Student.php
@@ -3,13 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Student extends Model
 {
+  use SoftDeletes;
+
+  /**
+   * Get the user model related with the student
+   */
+  public function user()
+  {
+    $this->morphOne('App\User', 'userable');
+  }
+
+  /**
+   * Get the department
+   */
   public function department(){
     return $this->belongsTo('App\Models\Department');
   }
 
+  /**
+   * Get the courses offered by the student
+   */
   public function courses(){
     return $this->belongsToMany('App\Models\Course')->withTimestamps();
   }
