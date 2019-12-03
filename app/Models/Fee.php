f07bb95c39367d7825ad5815077d965c200955b9
Iyiola-am <adekunle3317@gmail.com>
2019-12-03 22:58:28 +0100

    Added models for Fees and SystemSettings

--- app/Models/Fee.php
+++ app/Models/Fee.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fee extends Model
 {
-    //
+    protected $dates = ['created_at', 'updated_at', 'expiry_date'];
+
+    /**
+     * Get the department the fee belongs to
+     */
+    public function department()
+    {
+        return $this->belongsTo('App\Models\Department');
+    }
 }
