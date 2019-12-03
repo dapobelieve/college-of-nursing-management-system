0f335a9f7f56b634596712298d3c0687e0b648b2
Iyiola-am <adekunle3317@gmail.com>
2019-12-03 22:50:09 +0100

    Added fees and systems setting table migrations

---
+++ database/migrations/2019_12_03_205643_create_fees_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fees', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('department_id')->unsigned();
+            $table->enum('level', ['100', '200', '300', '400', '500']);
+            $table->decimal('indigene')->unsigned();
+            $table->decimal('non_indigene')->unsigned();
+            $table->timestamp('expiry_date');
+            $table->foreign('department_id')->references('id')->on('departments')->onDelete('CASCADE');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fees');
+    }
+}
---
+++ database/migrations/2019_12_03_205911_create_system_settings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSystemSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('system_settings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 255)->unique();
+            $table->text('value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('system_settings');
+    }
+}
