6e8eaadd11b9db73b905b8403ce4f2b05f0c7818
Iyiola-am <adekunle3317@gmail.com>
2019-12-12 01:27:53 +0100

    Access control added to super admin only routes

--- routes/web.php
+++ routes/web.php
@@ -121,9 +121,6 @@ Route::group(['prefix' => '/admin', 'namespace' => 'Admin', 'middleware' => 'rol
   // Dashboard
   Route::get('', 'DashboardController@index')->name('dashboard.home');
 
-  //cards
-  Route::resource('cards', 'CardController');
-  Route::get('/index2', 'CardController@index2')->name('cards.index2');
 
   // Courses
   Route::resource('courses', 'CourseController');
@@ -163,12 +160,6 @@ Route::group(['prefix' => '/admin', 'namespace' => 'Admin', 'middleware' => 'rol
   Route::get('/addresult/{id}', 'StudentController@showresult')->name('students.showresult');
   Route::post('/addresult', 'StudentController@addresult')->name('students.addresult');
 
-  // Cardapplicants sub_section
-  Route::resource('cardapplicants', 'CardapplicantController');
-  Route::get('/index3', 'CardapplicantController@index2')->name('cardapplicants.index3');
-  Route::post('/index3', 'CardapplicantController@exportcsv')->name('cardapplicants.exportcsv');
-  Route::post('cardapplicants/index', 'CardapplicantController@deleteall')->name('cardapplicants.deleteall');
-
   // Applicants section
   Route::get('applicants/index', 'ApplicantController@index')->name('applicants.index');
   Route::get('applicants/index/{studentapplicant}', 'ApplicantController@edit')->name('applicants.edit');
@@ -178,27 +169,39 @@ Route::group(['prefix' => '/admin', 'namespace' => 'Admin', 'middleware' => 'rol
 
   Route::put('applicants/addscore/{studentapplicant}', 'ApplicantController@update')->name('applicants.update')->middleware('checkAdminPermissions:super,intermediate');
 
+  // Super admin only routes
   Route::group(['middleware' => ['checkAdminPermissions:super']], function() {
+    // Cards
+    Route::resource('cards', 'CardController');
+    Route::get('/index2', 'CardController@index2')->name('cards.index2');
+
+    // Cardapplicants sub_section
+    Route::resource('cardapplicants', 'CardapplicantController');
+    Route::get('/index3', 'CardapplicantController@index2')->name('cardapplicants.index3');
+    Route::post('/index3', 'CardapplicantController@exportcsv')->name('cardapplicants.exportcsv');
+    Route::post('cardapplicants/index', 'CardapplicantController@deleteall')->name('cardapplicants.deleteall');
+
+    // Applicants
     Route::put('applicants/index', 'ApplicantController@deleteall')->name('applicants.deleteall');
     Route::delete('applicants/destroy/{studentapplicant}', 'ApplicantController@delete')->name('applicants.destroy');
     Route::get('applicants/confirmteller/{studentapplicant}', 'ApplicantController@tellerindex')->name('applicants.addtelleredit');
     Route::put('applicants/confirmteller/{studentapplicant}', 'ApplicantController@addteller')->name('applicants.addteller');
+
+    // Admins section
+    Route::resource('admins', 'AdminController',  [
+      'only' => [
+        'index', 'create', 'store', 'edit', 'update', 'show'
+      ],
+      'parameters' => [
+        'admins' => 'admin'
+      ]
+    ]);
+
+    // Roles section
+    Route::get('roles', 'RoleController@index');
+
+    // System settings
+    Route::get('settings', 'SettingController@index')->name('settings.index');
+    Route::put('settings/update', 'SettingController@update')->name('settings.update');
   });
-
-  // Admins section
-  Route::resource('admins', 'AdminController',  [
-    'only' => [
-      'index', 'create', 'store', 'edit', 'update', 'show'
-    ],
-    'parameters' => [
-      'admins' => 'admin'
-    ]
-  ])->middleware('checkAdminPermissions:super');
-
-  // Roles section
-  Route::get('roles', 'RoleController@index');
-
-  // System settings
-  Route::get('settings', 'SettingController@index')->name('settings.index');
-  Route::put('settings/update', 'SettingController@update')->name('settings.update');
 });
